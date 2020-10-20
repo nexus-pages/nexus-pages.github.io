f34617f9d315b9193b2550439f78585b24cb111a
nexus-pages <sujal.mandal.india@outlook.com>
2020-10-20 15:25:41 +0530

    initial commit

---
+++ torn-trade/precache-manifest.fc7224950a23bd8f93dfb53c62a5ac15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533145ec70500476cbf7219b4191bb2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62a9064d4d6c9fcf9dea",
+    "url": "/static/css/2.5d8ac9a3.chunk.css"
+  },
+  {
+    "revision": "62a9064d4d6c9fcf9dea",
+    "url": "/static/js/2.d1eb3d35.chunk.js"
+  },
+  {
+    "revision": "5281e5955224e3425f6dbcef3ec17696",
+    "url": "/static/js/2.d1eb3d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d7fe75dd18348b9d9e",
+    "url": "/static/js/main.6a2e0b4a.chunk.js"
+  },
+  {
+    "revision": "be6ad062b5909b22ed2c",
+    "url": "/static/js/runtime-main.4c3710f9.js"
+  }
+]);
